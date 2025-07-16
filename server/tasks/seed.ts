3b2e1275d719a7987a418aea560c9217b172d387
TusharW4ni <reachtusharwani@gmail.com>
2025-07-16 07:14:23 -0500

    connected remote and local database so can run seed through local

--- server/tasks/seed.ts
+++ server/tasks/seed.ts
@@ -1,33 +1,27 @@
-// import { tables } from "../utils/drizzle";
-
-// export default defineTask({
-//   meta: {
-//     name: "db:seed",
-//     description: "Run database seed task",
-//   },
-async function run() {
-  console.log("Running DB seed task...");
-  const users = [
-    {
-      name: "John Doe",
-      email: "john@example.com",
-      password: "password123",
-      avatar: "https://example.com/avatar/john.png",
-      createdAt: new Date(),
-    },
-    {
-      name: "Jane Doe",
-      email: "jane@example.com",
-      password: "password123",
-      avatar: "https://example.com/avatar/jane.png",
-      createdAt: new Date(),
-    },
-  ];
-  await useDrizzle().insert(tables.users).values(users);
-  return { result: "success" };
-}
-// });
-run().catch((e) => {
-  console.error(e);
-  process.exit(1);
+export default defineTask({
+  meta: {
+    name: "db:seed",
+    description: "Run database seed task",
+  },
+  async run() {
+    console.log("Running DB seed task...");
+    const users = [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+        password: "password123",
+        avatar: "https://example.com/avatar/john.png",
+        createdAt: new Date(),
+      },
+      {
+        name: "Jane Doe",
+        email: "jane@example.com",
+        password: "password123",
+        avatar: "https://example.com/avatar/jane.png",
+        createdAt: new Date(),
+      },
+    ];
+    await useDrizzle().insert(tables.users).values(users);
+    return { result: "success" };
+  },
 });
